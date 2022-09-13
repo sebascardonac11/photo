1bdca00f0fc3a1e7847db19ecf9abc47f3aedebd
Sebastian Cardona Correa <sebascardonac11@gmail.com>
2022-09-13 10:01:40 -0500

    Initial version

---
+++ functions/photo.js
@@ -0,0 +1,28 @@
+const AWS = require('aws-sdk');
+module.exports = class Photo {
+    async putPhoto(fileName, contentType, body, email) {
+        try {
+            var filePath = "photoClient/" + fileName
+            var params = {
+                Bucket: "photoevent",
+                Body: body,
+                Key: filePath,
+                ContentType: contentType,
+                Metadata: {
+                    "Photographer": email
+                }
+            };
+            var photo = await s3Client.upload(params).promise();
+            return {
+                statusCode: 200,
+                data: photo
+            }
+        } catch (error) {
+            console.log("Something wrong in session.putPhoto: ", error)
+            return {
+                statusCode: 404,
+                data: error
+            }
+        }
+    }
+}
