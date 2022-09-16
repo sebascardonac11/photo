5dbee2235183999c35b14df3b27aa03c24b35c83
Sebastian Cardona Correa <sebascardonac11@gmail.com>
2022-09-16 18:23:38 -0500

    Handle Bucket S3 to Analyse

--- functions/photo.js
+++ functions/photo.js
@@ -2,17 +2,19 @@ const AWS = require('aws-sdk');
 const s3Client = new AWS.S3();
 const dynamo = new AWS.DynamoDB.DocumentClient();
 
+const AnalyzePhoto = require('./analyzePhoto')
+
 module.exports = class Photo {
     BUCKET;
     DYNAMODBTABLE;
-    constructor(bucket,table) {
+    constructor(bucket, table) {
         this.BUCKET = bucket;
         this.DYNAMODBTABLE = table
-      }
+    }
 
-    async putPhoto(fileName, contentType, body, email,event,session) {
+    async putPhoto(fileName, contentType, body, email, event, session) {
         try {
-            var filePath = "photoClient/" +event+"/"+session+"/"+ fileName
+            var filePath = "photoClient/" + event + "/" + session + "/" + fileName
             var params = {
                 Bucket: this.BUCKET,
                 Body: body,
@@ -22,9 +24,9 @@ module.exports = class Photo {
                     "Photographer": email
                 }
             };
-            var item={'photographer':email,'session':session,'key':fileName}
-            var photoDB=await this.insertPhotoDB(item);
-            console.log('DB: ',photoDB);
+            var item = { 'photographer': email, 'session': session, 'key': fileName }
+            var photoDB = await this.insertPhotoDB(item);
+            console.log('DB: ', photoDB);
             var photo = await s3Client.upload(params).promise();
             return {
                 statusCode: 200,
@@ -38,7 +40,6 @@ module.exports = class Photo {
             }
         }
     }
-
     async insertPhotoDB(Item) {
         try {
             var params = {
@@ -50,5 +51,37 @@ module.exports = class Photo {
             console.log("Something wrong in photo.insertPhotoDB: ", error);
         }
     }
-
+    async analyzePhoto(bucketName, Key) {
+        try {
+            var detectPhotos = new AnalyzePhoto(bucketName, Key);
+            var labels = await detectPhotos.getLabel();
+            var texts = await detectPhotos.getText();
+            var params2 = {
+                Bucket: bucketName,
+                Key: Key,
+                Tagging: {
+                    TagSet: [
+                        {
+                            Key: "Labels",
+                            Value: labels
+                        },
+                        {
+                            Key: "Texts",
+                            Value: texts
+                        }]
+                }
+            };
+            var response =s3.putObjectTagging(params2, function (err, data) { if (err) console.log(err, err.stack); });
+            return {
+                statusCode: 200,
+                data: response
+            }
+        } catch (error) {
+            console.log("Something wrong in photo.putPhoto: ", error)
+            return {
+                statusCode: 404,
+                data: error
+            }
+        }
+    }
 }
--- index.js
+++ index.js
@@ -2,33 +2,37 @@ const Photo = require('./functions/photo')
 const jwt_decode = require('jwt-decode');
 const parser = require('lambda-multipart-parser');
 exports.handler = async function (event, context, callback) {
-    console.log("Event Photo: ", event);
-    console.log("Context Photo: ", context);
-    console.log("callback Photo: ", callback);
-    var photo = new Photo(process.env.BUCKET,process.env.DYNAMODB);
-    var response={statusCode: 401,data: "Whitout Information"};
-    switch (event.httpMethod) {
-        case 'PUT':
-            console.log("### PUT ####");
-            var authorizationDecoded = jwt_decode(event.headers.Authorization);
-            const form = await parser.parse(event);
-            var key = form.event + '/' + form.session + '/' + form.files[0].filename;
-            var contenType = form.files[0].contentType;
-            var body = Buffer.from(form.files[0].content);
-            response = await photo.putPhoto(form.files[0].filename, contenType, body, authorizationDecoded.email,form.event,form.session);
-            break;
-        default:
-              console.log("Without httpMethod");
-    }
-    console.log("Response: ", response);
-    return {
-      statusCode: response.statusCode,
-      headers: {
-        "Access-Control-Allow-Headers": "Content-Type",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "OPTIONS,POST,GET,PUT",
-        "Content-Type": 'application/json'
-      },
-      body: JSON.stringify(response.data)
-    };
+  console.log("Event Photo: ", JSON.stringify(event));
+  var photo = new Photo(process.env.BUCKET, process.env.DYNAMODB);
+  var response = { statusCode: 401, data: "Whitout Information" };
+  switch (event.httpMethod) {
+    case 'PUT':
+      console.log("### PUT ####");
+      var authorizationDecoded = jwt_decode(event.headers.Authorization);
+      const form = await parser.parse(event);
+      var key = form.event + '/' + form.session + '/' + form.files[0].filename;
+      var contenType = form.files[0].contentType;
+      var body = Buffer.from(form.files[0].content);
+      response = await photo.putPhoto(form.files[0].filename, contenType, body, authorizationDecoded.email, form.event, form.session);
+      break;
+    default:
+      event.Records.forEach(async Record => {
+        const Key = Record.s3.object.key;
+        const user = Record.userIdentity;
+        const bucketName = Record.s3.bucket.name;
+        photo.analyzePhoto(bucketName,Key);
+      });
+
+  }
+  console.log("Response: ", response);
+  return {
+    statusCode: response.statusCode,
+    headers: {
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "OPTIONS,POST,GET,PUT",
+      "Content-Type": 'application/json'
+    },
+    body: JSON.stringify(response.data)
+  };
 }
