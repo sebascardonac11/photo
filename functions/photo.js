06ca57f29468dd402070ed0d0787dd258daa2bc5
Sebastian Cardona Correa <sebascardonac11@gmail.com>
2022-09-16 18:39:01 -0500

    Handle Bucket S3 to Analyse 6

--- functions/photo.js
+++ functions/photo.js
@@ -52,7 +52,7 @@ module.exports = class Photo {
         }
     }
     async analyzePhoto(bucketName, Key) {
-        console("Analysing key: ",key);
+        console("Analysing key: ",Key);
         try {
             var detectPhotos = new AnalyzePhoto(bucketName, Key);
             var labels = await detectPhotos.getLabel();
