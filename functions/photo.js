d12247482c288015221a042c39532e4946fb7553
Sebastian Cardona Correa <sebascardonac11@gmail.com>
2022-09-16 14:53:02 -0500

    Add enviaroment variable to lambda

--- functions/photo.js
+++ functions/photo.js
@@ -45,7 +45,7 @@ module.exports = class Photo {
                 TableName: this.DYNAMODBTABLE,
                 Item: Item
             }
-            return await dynamo.put(params).promise();
+            return await dynamo.putItem(params).promise();
         } catch (error) {
             console.log("Something wrong in photo.insertPhotoDB: ", error);
         }
