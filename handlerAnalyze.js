e042f749b3ded72c4971c31c5e4abe994b23e5b6
Sebastian Cardona Correa <sebascardonac11@gmail.com>
2022-09-16 18:45:51 -0500

    Handle Bucket S3 to Analyse 9

--- handlerAnalyze.js
+++ handlerAnalyze.js
@@ -1,5 +1,5 @@
 const AWS = require('aws-sdk');
-AWS.config.update({ region: 'us-east-2' });
+//AWS.config.update({ region: 'us-east-2' });
 const s3 = new AWS.S3()
 const detectPhoto = require('./functions/analyzePhoto');
 
