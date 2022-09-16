67ef74a98fa4e174557cd167fd822bff118c3aa2
Sebastian Cardona Correa <sebascardonac11@gmail.com>
2022-09-16 10:56:01 -0500

    V1.5

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const parser = require('lambda-multipart-parser');
 exports.handler = async function (event, context, callback) {
     console.log("Event Photo: ", event);
     var photo = new Photo();
+    var response;
     var authorizationDecoded = jwt_decode(event.headers.Authorization);
-    this.response.statusCode=401;
-    this.response.data="Testing"
+    response.statusCode=401;
+    response.data="Testing"
     /*switch (event.httpMethod) {
         case 'PUT':
             console.log("### PUT ####");
@@ -14,20 +15,20 @@ exports.handler = async function (event, context, callback) {
             var key = form.event + '/' + form.session + '/' + form.files[0].filename;
             var contenType = form.files[0].contentType;
             var body = Buffer.from(form.files[0].content);
-            this.response = await photo.putPhoto(key, contenType, body, authorizationDecoded.email);
+            response = await photo.putPhoto(key, contenType, body, authorizationDecoded.email);
             break;
         default:
               console.log("Without httpMethod");
     }*/
-    console.log("Response: ", this.response);
+    console.log("Response: ", response);
     return {
-      statusCode: this.response.statusCode,
+      statusCode: response.statusCode,
       headers: {
         "Access-Control-Allow-Headers": "Content-Type",
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "OPTIONS,POST,GET,PUT",
         "Content-Type": 'application/json'
       },
-      body: JSON.stringify(this.response.data)
+      body: JSON.stringify(response.data)
     };
 }
