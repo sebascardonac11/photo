036df577af84a6407b1aa4c982f5102ffc4d11f3
Sebastian Cardona Correa <sebascardonac11@gmail.com>
2022-09-16 18:53:55 -0500

    Handle Bucket S3 to Analyse 10

--- functions/analyzePhoto.js
+++ functions/analyzePhoto.js
@@ -1,41 +1,46 @@
 const AWS = require('aws-sdk')
-function analyzePhoto(bucket,photo){
-        this.bucket=bucket;
-        this.photo=photo;
-        const client = new AWS.Rekognition();
-  this.getLabel = async function(){
-    const params = {
-              Image: {
-                  S3Object: {
-                      Bucket: bucket,
-                      Name: photo
-                  },
-               },
-              MaxLabels: 10
-          }
-    var labelsTags = ""
-    const data = await client.detectLabels(params).promise();
-    data.Labels.forEach(label => {
-     if (labelsTags.search(label.Name)==-1)
-                labelsTags+=label.Name + "-";
-           });
-    return labelsTags;
-    };
-  this.getText = async function(){
-        const paramsText = {
-          Image: {
-            S3Object: {
+function analyzePhoto(bucket, photo) {
+  this.bucket = bucket;
+  this.photo = photo;
+  const client = new AWS.Rekognition();
+  this.getLabel = async function () {
+    try {
+      const params = {
+        Image: {
+          S3Object: {
             Bucket: bucket,
             Name: photo
-            }}
-        };
-        const data = await client.detectText(paramsText).promise();
-        var texts=""
-         data.TextDetections.forEach(text => {
-            if (texts.search(text.DetectedText)==-1)
-                  texts+=text.DetectedText + "-";
-         });
-        return texts;
+          },
+        },
+        MaxLabels: 10
+      }
+      var labelsTags = ""
+      const data = await client.detectLabels(params).promise();
+      data.Labels.forEach(label => {
+        if (labelsTags.search(label.Name) == -1)
+          labelsTags += label.Name + "-";
+      });
+      return labelsTags;
+    } catch (error) {
+      console.log("Something wrong in analyzePhoto.getLabel: ", error)
+    }
+  };
+  this.getText = async function () {
+    const paramsText = {
+      Image: {
+        S3Object: {
+          Bucket: bucket,
+          Name: photo
+        }
+      }
     };
+    const data = await client.detectText(paramsText).promise();
+    var texts = ""
+    data.TextDetections.forEach(text => {
+      if (texts.search(text.DetectedText) == -1)
+        texts += text.DetectedText + "-";
+    });
+    return texts;
+  };
 }
 module.exports = analyzePhoto
--- functions/photo.js
+++ functions/photo.js
@@ -1,6 +1,6 @@
 const AWS = require('aws-sdk');
 const s3Client = new AWS.S3();
-AWS.config.update({ region: 'us-east-2' });
+//AWS.config.update({ region: 'us-east-2' });
 const dynamo = new AWS.DynamoDB.DocumentClient();
 
 const AnalyzePhoto = require('./analyzePhoto')
